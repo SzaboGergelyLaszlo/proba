1c562c251139d1b42776e77a86ed827b3d451cd3
SzaboGergelyLaszlo <144820446+SzaboGergelyLaszlo@users.noreply.github.com>
2025-05-29 08:51:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace celloveszetCLI
+{
+    public class Program
+    {
+        //8. feladat
+        public static int Legnagyobb(int a, int b, int c, int d)
+        {
+            int Legnagyobb = a;
+            List<int> lovesek = new List<int>();
+            lovesek.Add(a);
+            lovesek.Add(b);
+            lovesek.Add(c);
+            lovesek.Add(d);
+            foreach (var i in lovesek)
+            {
+                if (i > Legnagyobb)
+                {
+                    Legnagyobb = i;
+                }
+            }
+
+            return Legnagyobb;
+        }
+
+         static List<Lovo> lovok = new List<Lovo>();
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("lovesek.csv");
+            while (!sr.EndOfStream)
+            {
+                lovok.Add(new Lovo(sr.ReadLine()));
+            }
+            sr.Close();
+
+            //9.feladat
+            foreach (var i in lovok)
+            {
+                Console.WriteLine(i.Nev + " " + Legnagyobb(i.ElsoLoves, i.MasodikLoves, i.HarmadikLoves, i.NegyedikLoves));
+            }
+
+            //10. feladat
+
+            int index = 0;
+            for (int i = 0; i < lovok.Count; i++)
+            {
+                if (Legnagyobb(lovok[index].ElsoLoves, lovok[index].MasodikLoves, lovok[index].HarmadikLoves, lovok[index].NegyedikLoves) < Legnagyobb(lovok[i].ElsoLoves, lovok[i].MasodikLoves, lovok[i].HarmadikLoves, lovok[i].NegyedikLoves))
+                {
+                    index = i;
+                }
+            }
+
+            //11.feladat
+
+            int indexAtlag = 0;
+
+            for (int i = 0; i < lovok.Count; i++)
+            {
+                if (Lovo.atlag(lovok[indexAtlag]) > Lovo.atlag(lovok[i]))
+                {
+                    indexAtlag = i;
+                }
+            }
+            
+
+            Console.WriteLine("A legnagyobb találatott elért eredményei: " + lovok[index].ToString());
+            Console.WriteLine("A legkisebb átlagú eredményei: " + lovok[indexAtlag].Nev + " " + Lovo.atlag(lovok[indexAtlag]));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ celloveszetCLITests/ProgramTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using celloveszetCLI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace celloveszetCLI.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        [DataRow(22, 29, 12, 23, 29)]
+        [DataRow(16, 45, 87, 33, 87)]
+        [DataRow(96, 49, 67, 45, 96)]
+        [DataRow(44, 3, 12, 77, 77)]
+
+
+        public void LegnagyobbTest(int elsoLoves, int masodikLoves, int harmadikLoves, int negyedikLoves, int expected)
+        {
+            Assert.AreEqual(expected, Program.Legnagyobb(elsoLoves, masodikLoves, harmadikLoves, negyedikLoves));
+        }
+    }
+}
